40f70415e90b9bc8921555d9547f006f54433a44
Sahil <sahilsarin390@gmail.com>
2022-01-20 19:39:17 +0530

    If-else ladder

---
+++ Quest1/control-flow/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+     //if-else-if ladder
+
+     let num = 2;
+     if num == 1 {
+         println!("You Won!!")
+     } 
+     else if num == 2 {
+        println!("Try Again!")
+     }
+     else {
+         println!("You lost.")
+     }
+}
