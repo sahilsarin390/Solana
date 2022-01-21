b3273fcccb0b0144b8f6d16c699fd008c4ae5952
Sahil <sahilsarin390@gmail.com>
2022-01-21 18:59:48 +0530

    Quest3

---
+++ Quest3/roulette/helper.js
@@ -0,0 +1,27 @@
+function getReturnAmount(expectedTokenToStake, ratioOfStake){
+    try{
+        return expectedTokenToStake*ratioOfStake;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+// // Get total Amount that is to be paid by the player for each game
+function totalAmtToBePaid(expectedTokenToStake){
+    try{
+        return expectedTokenToStake;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+// generate a random integer between [min, max]
+function randomNumber(min, max){
+    try{
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports={getReturnAmount, totalAmtToBePaid, randomNumber};
